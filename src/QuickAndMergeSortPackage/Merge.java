7c124fd3d3e7d4c42de1123b49f647e7f0a3341f
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-09-20 16:31:07 -0300

    Merge completed.

--- src/QuickAndMergeSortPackage/Merge.java
+++ src/QuickAndMergeSortPackage/Merge.java
@@ -1,4 +1,3 @@
-package QuickAndMergeSortPackage;
 
 public class Merge {
 
@@ -28,6 +27,16 @@ public class Merge {
         assert isSorted(a, lo, hi);
     }
 
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length -1);
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
     private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
         if (hi <= lo) return;
         int mid = lo + (hi - lo) / 2;
@@ -38,7 +47,32 @@ public class Merge {
 
     public static void sort(Comparable[] a) {
         Comparable[] aux = new Comparable[a.length];
-        sort(a, aux, 0, a.length0-1);
+        sort(a, aux, 0, a.length-1);
         assert isSorted(a);
      }
+
+    // is v < w?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    // print array to stantard output
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        Merge.sort(a);
+        show(a);
+    }
 }
