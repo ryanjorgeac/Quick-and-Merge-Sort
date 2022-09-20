ff5edd7591a9b17a40ca5e15fe3cc77842e894b6
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-09-20 16:21:19 -0300

    Quick and Merge sort.

---
+++ src/QuickAndMergeSortPackage/Merge.java
@@ -0,0 +1,44 @@
+package QuickAndMergeSortPackage;
+
+public class Merge {
+
+    // This class should not be instatiated.
+    private Merge() { }
+
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        // precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
+        assert isSorted(a, lo, mid);
+        assert isSorted(a, mid+1, hi);
+
+        // copy to aux[]
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        //merge back to a[]
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)              a[k] = aux[j++];
+            else if (j > hi)               a[k] = aux[i++];
+            else if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else                           a[k] = aux[i++];
+        }
+
+        //postcondition: a[lo .. hi] is sorted
+        assert isSorted(a, lo, hi);
+    }
+
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length0-1);
+        assert isSorted(a);
+     }
+}
---
+++ src/QuickAndMergeSortPackage/Quick.java
@@ -0,0 +1,97 @@
+
+
+public class Quick {
+
+    private Quick() { }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+        assert isSorted(a);
+    }
+
+    //quicksort the subarray from a[lo] to a[hi]
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+        assert isSorted(a, lo, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+
+            //find item on lo to swap
+            while (less(a[++i], v))
+                if (i == hi) break;
+
+            //find item on hi to swap
+            while (less(v, a[--j]))
+                if (j == lo) break;
+
+            // check if pointers cross
+            if (i >= j) break;
+
+            exch(a, i, j);
+        }
+
+        // put partitioning item v at a[j]
+        exch(a, lo, j);
+
+        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+        return j;
+    }
+
+    /********************************************************
+     * Helper sorting functions.
+     ********************************************************/
+
+    // is v < w ?
+    private static boolean less (Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    /********************************************************
+     * Check if array is sorted - useful for debugging.
+     ********************************************************/
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length -1);
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+    //print array to standart output
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        Quick.sort(a);
+        show(a);
+
+        // shuffle
+        StdRandom.shuffle(a);
+
+        // display results again using select
+        StdOut.println();
+    }
+}
+
